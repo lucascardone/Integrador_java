e3758d60556b5a5b0f35b1a013576528579a7674
Lucas Cardone <lucas.a.cardone@gmail.com>
2022-04-11 15:40:18 -0300

    Beta method: distancia -> improved

--- src/implement/RadarImplementService.java
+++ src/implement/RadarImplementService.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package implement;
 
 import entity.Objetos;
@@ -11,30 +10,42 @@ import enums.NvlResistencia;
 import java.util.ArrayList;
 import java.util.Random;
 
-
 public class RadarImplementService {
 
-    public ArrayList<Objetos> pulsoRadar(){
+    public ArrayList<Objetos> pulsoRadar() {
         System.out.println("----------Pulso Radar----------");
         String letra = "A";
         ArrayList<Objetos> objetos = new ArrayList();
         Random random = new Random();
-        int cantidad = (int) (Math.random()*10);
+        int cantidad = (int) (Math.random() * 10);
         for (int i = 0; i < cantidad; i++) {
             int nvlResis = random.nextInt(NvlResistencia.values().length);
-            objetos.add(new Objetos(random.nextBoolean(), letra.concat(String.valueOf(i)), NvlResistencia.values()[nvlResis], 100, (int)(Math.random()*1000), (int)(Math.random()*1000), (int)(Math.random()*1000)));
+            objetos.add(new Objetos(random.nextBoolean(), letra.concat(String.valueOf(i)), NvlResistencia.values()[nvlResis], 100, (int) (Math.random() * 1000), (int) (Math.random() * 1000), (int) (Math.random() * 1000)));
         }
         return objetos;
     }
-    
-   
-    public Integer distancia(Integer x1, Integer y1, Integer z1, Integer x2, Integer y2, Integer z2){
-        Integer x3 = 0 - x1 - 0 - x2;
-        x3 = (int)(Math.pow(x3, 2));
-        Integer y3 = 0 - y1 - 0 - y2;
-        y3 = (int)(Math.pow(y3, 2));
-        Integer z3 = 0 - z1 - 0 - z2;
-        z3 = (int)(Math.pow(z3, 2));
-        return (int)(Math.sqrt(x3 + y3 + z3));
+
+    public Integer distancia(Integer x1, Integer y1, Integer z1, Integer x2, Integer y2, Integer z2) {
+        Integer x3, y3, z3;
+        if (x1 != null && x2 != null && y1 != null && y2 != null && z1 != null && z2 != null) {
+            x3 = 0 - x1 - 0 - x2;
+            x3 = (int) (Math.pow(x3, 2));
+            y3 = 0 - y1 - 0 - y2;
+            y3 = (int) (Math.pow(y3, 2));
+            z3 = 0 - z1 - 0 - z2;
+            z3 = (int) (Math.pow(z3, 2));
+            return (int) (Math.sqrt(x3 + y3 + z3));
+        }else if (x1 != null && x2 != null && z1 != null && z2 != null && y1 == null && y2 == null){
+            x3 = 0 - x1 - 0 - x2;
+            x3 = (int) (Math.pow(x3, 2));
+            z3 = 0 - z1 - 0 - z2;
+            z3 = (int) (Math.pow(z3, 2));
+            return (int) (Math.sqrt(x3 + z3));
+        }else if(x1 == null && x2 == null && y1 != null && y2 != null && z1 == null && z2 == null){
+            y3 = 0 - y1 - 0 - y2;
+            y3 = (int) (Math.pow(y3, 2));
+            return (int) (Math.sqrt(y3));
+        }
+        return 0;
     }
 }
